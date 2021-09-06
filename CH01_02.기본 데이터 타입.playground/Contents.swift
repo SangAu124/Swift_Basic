88ff6d79684a87e192274a8a59fe1c71ab6cc5fe
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-09-07 02:46:49 +0900

    CH01_02.기본 데이터 타입
    기본 데이터 타입을 공부하였습니다.

---
+++ CH01_02.기본 데이터 타입.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+// - Int: 64bit 정수형
+// - UInt: 부호가 없는 64bit 정수형
+// - Float: 32bit 부동 소수점
+// - Double: 64bit 부동 소수점
+// - Bool: true, false 값
+// - Character: 문자
+// - String: 문자열
+// - Any: 모든 타입을 지칭하는 키워드
+
+//Int
+var someInt: Int = -100
+someInt = 100
+
+//UInt
+var someUInt: UInt = 200
+
+//Float
+var someFloat: Float = 1.1
+someFloat = 1
+print(someFloat)
+
+//Double
+var someDouble: Double = 1.1
+someDouble = 1
+
+//Bool
+var someBool: Bool = true
+someBool = false
+
+//Character
+var someCharacter: Character = "가"
+someCharacter = "A"
+someCharacter = "👏"
+
+//String
+var someString: String = "안녕하세요 👏"
+
+//타입 추론
+var number = 10
