e21323a070cce546e3cf8c0930374500e813a0a6
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-09-07 19:18:53 +0900

    CH01_07.옵셔널
    옵셔널을 공부 했습니다.

---
+++ CH01_07.옵셔널.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//옵셔널 -> 값이 있을 수도 있고 없을 수도 있다.
+
+//var name: String = ""   -> 값이 없는게 아니다
+//var name: String? = nil  -> 값이 없다 (nil == null)
+
+var name: String?
+
+var optionalName: String? = "SangAu"
+print(optionalName)
+
+// var requiredName: String = optionalName
