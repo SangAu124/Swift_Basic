9dba4674095c08f56065cb491201d8fc60a62aa6
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-09-21 22:58:29 +0900

    CH02_06.열거형 다시공부
    열거형을 다시 공부하였습니다.

--- CH02_06.열거형.playground/Contents.swift
+++ CH02_06.열거형.playground/Contents.swift
@@ -18,12 +18,31 @@ import UIKit
  }
  */
 
-
-enum Dinner {
-    case pizza
-    case rice
-    case kimbab
+enum Weekday {
+    case mon
+    case tue
+    case wed
+    case thu, fri, sat, sun
 }
 
-let meal: Dinner = Dinner.kimbab
-print(meal)
+// 열거형 타입과 케이스를 모두 사용하여도 됩니다
+var day: Weekday = Weekday.mon
+
+// 타입이 명확하다면 .케이스 처럼 표현해도 무방합니다
+day = .tue
+
+print(day) // tue
+
+// switch의 비교값에 열거형 타입이 위치할 때
+// 모든 열거형 케이스를 포함한다면
+// default를 작성할 필요가 없습니다
+switch day {
+case .mon, .tue, .wed, .thu:
+    print("평일입니다")
+case Weekday.fri:
+    print("불금 파티!!")
+case .sat, .sun:
+    print("신나는 주말!!")
+}
+// 평일입니다
+
